2a017a5c5620470492d2b64d91a43bb0d9143739
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2023-09-11 00:58:42 +0100

    add the tasks model

---
+++ TaskManagement.API/Models/Tasks.cs
@@ -0,0 +1,19 @@
+﻿using TaskManagement.API.Common;
+
+namespace TaskManagement.API.Models;
+
+public class Tasks : BaseEntity
+{
+    public string Title { get; set; } = default!;
+    public string Description { get; set; } = default!;
+    public DateTime? DueDate { get; set; }
+    public TasksPriority Priority { get; set; }
+
+    public TasksStatus Status { get; set; }
+
+    public int ProjectId { get; set; }
+    public Project Project { get; set; }
+
+    public int UserId { get; set; }
+    public User User { get; set; }
+}
