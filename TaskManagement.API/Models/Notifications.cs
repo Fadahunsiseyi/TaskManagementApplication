828e3fe70df4b7ceb2bcf7eb137a7a92e0ddd4d5
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2023-09-11 00:59:46 +0100

    add the notification model

---
+++ TaskManagement.API/Models/Notifications.cs
@@ -0,0 +1,11 @@
+﻿using TaskManagement.API.Common;
+
+namespace TaskManagement.API.Models;
+
+public class Notifications : BaseEntity
+{
+    public int Id { get; set; }
+    public NotificationsType Type { get; set; }
+    public string Message { get; set; }
+    public DateTime Timestamp { get; set; }
+}
