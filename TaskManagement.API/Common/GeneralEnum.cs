c439c100c5379fab913c446a59c7290701b8e693
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2023-09-11 00:20:13 +0100

    add project,task and notification enum

---
+++ TaskManagement.API/Common/GeneralEnum.cs
@@ -0,0 +1,25 @@
+﻿namespace TaskManagement.API.Common
+{
+    // Enum 1
+    public enum TasksPriority
+    {
+        Low,
+        Medium,
+        High
+    }
+
+    // Enum 2
+    public enum TasksStatus
+    {
+        Pending,
+        InProgress,
+        Completed
+    }
+
+    // Enum 3
+    public enum NotificationsType
+    {
+        DueDateReminder,
+        StatusUpdate
+    }
+}
