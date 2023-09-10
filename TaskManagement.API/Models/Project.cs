2fe612063f5a1e0da96997776443b84c17db5af4
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2023-09-11 00:59:09 +0100

    add the project model

---
+++ TaskManagement.API/Models/Project.cs
@@ -0,0 +1,8 @@
+﻿namespace TaskManagement.API.Models;
+
+public class Project : BaseEntity
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public List<Tasks> Tasks { get; set; }
+}
