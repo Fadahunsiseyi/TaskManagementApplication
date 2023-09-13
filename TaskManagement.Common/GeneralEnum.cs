59e7a04ff5395482cc1bb998c5bbb93400d5fd3c
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2023-09-13 17:33:28 +0100

    add all the enums needed

---
+++ TaskManagement.Common/GeneralEnum.cs
@@ -0,0 +1,25 @@
+﻿namespace TaskManagement.Common.Enums
+{
+    // Enum 1
+    public enum TasksPriority
+    {
+        Low,
+        Medium,
+        High
+    }
+
+    // Enum 2
+    public enum TasksStatus
+    {
+        Pending,
+        InProgress,
+        Completed
+    }
+
+    // Enum 3
+    public enum NotificationsType
+    {
+        DueDateReminder,
+        StatusUpdate
+    }
+}
