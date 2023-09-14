6e2eb816002830f84b417c79b22faf1f1eff55ed
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2023-09-15 00:38:24 +0100

    create endpoint to create a particular user

---
+++ TaskManagement.API/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskManagement.Domain.Dtos.User;
+using TaskManagement.Domain.Interface.Services;
+
+namespace TaskManagement.API.Controllers;
+
+
+[ApiController]
+[Route("[controller]")]
+public class UserController : ControllerBase
+{
+    public IUserService UserService { get; }
+
+    public UserController(IUserService userService)
+    {
+        UserService = userService;
+    }
+
+    [HttpPost]
+    [Route("Create")]
+
+    public async Task<IActionResult> Create(UserCreate userCreate)
+    {
+        var id = await UserService.CreateUserAsync(userCreate);
+        return Ok(id);
+    }
+    
+
+}
