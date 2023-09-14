41d87c13bb0607b9bbe5dd6bc21542e040d777cb
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2023-09-15 00:37:37 +0100

    implement the logic for the dto and entity map

---
+++ TaskManagement.Application/DtoEntityMapperProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagement.Domain.Dtos.User;
+using TaskManagement.Domain.Entities;
+
+namespace TaskManagement.Application
+{
+    public class DtoEntityMapperProfile : Profile
+    {
+        public DtoEntityMapperProfile()
+        {
+            CreateMap<UserCreate, User>()
+    .ForMember(dest => dest.Id, opt => opt.Ignore());
+        }
+    }
+}
