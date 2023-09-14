059064fd5df5c4f3b8ab888c6fcdd145b471a7a6
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2023-09-15 00:33:17 +0100

    implement the interface for the generic repo, add the create and save methods

---
+++ TaskManagement.Persistence/GenericRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManagement.Domain.Interface.Persistence;
+using TaskManagement.Domain.Entities;
+
+namespace TaskManagement.Persistence
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+    {
+        private ApplicationDbContext ApplicationDbContext { get; }
+        private DbSet<T> DbSet { get; }
+        public GenericRepository(ApplicationDbContext applicationDbContext)
+        {
+            ApplicationDbContext = applicationDbContext;
+            DbSet = applicationDbContext.Set<T>();
+        }
+       public async Task<Guid> InsertAsync(T entity)
+        {
+            entity.Created = DateTime.UtcNow;
+            await DbSet.AddAsync(entity);
+            return entity.Id;
+        }
+
+        public async System.Threading.Tasks.Task SaveChangesAsync()
+        {
+            await ApplicationDbContext.SaveChangesAsync();
+        }
+    }
+}
