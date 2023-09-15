7728617997f0d4902088db724ebe6a464b7833f1
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2023-09-16 00:57:15 +0100

    implement the endpoint to create a project

---
+++ TaskManagement.API/Controllers/ProjectController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskManagement.Domain.Dtos.Project;
+using TaskManagement.Domain.Interface.Services;
+
+namespace TaskManagement.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ProjectController : ControllerBase
+{
+    public IProjectService ProjectService { get; }
+
+    public ProjectController(IProjectService projectService)
+    {
+        ProjectService = projectService;
+    }
+
+    [HttpPost]
+    [Route("Create")]
+    public async Task<IActionResult> CreateProject(ProjectCreate projectCreate)
+    {
+        var id = await ProjectService.CreateProjectAsync(projectCreate);
+        return Ok(id);
+    }
+}
