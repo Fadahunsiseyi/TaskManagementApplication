3736131181d340da51bd45a023519cbf0ad12cf3
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2023-09-15 02:00:02 +0100

    contract to get all the users

--- TaskManagement.Domain/Interface/Persistence/IGenericRepository.cs
+++ TaskManagement.Domain/Interface/Persistence/IGenericRepository.cs
@@ -1,9 +1,11 @@
-﻿using TaskManagement.Domain.Entities;
+﻿using System.Linq.Expressions;
+using TaskManagement.Domain.Entities;
 
 namespace TaskManagement.Domain.Interface.Persistence;
 
 public interface IGenericRepository<T> where T : BaseEntity
 {
     Task<Guid> InsertAsync(T entity);
+    Task<IEnumerable<T>> GetAllAsync(int? skip, int? take);
     System.Threading.Tasks.Task SaveChangesAsync();
 }
