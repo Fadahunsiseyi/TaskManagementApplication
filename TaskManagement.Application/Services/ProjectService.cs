7af97d1e7ae1d97559689e00d33fdc5a393aca4e
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2023-09-16 00:56:25 +0100

    implement the create project method in the project service

---
+++ TaskManagement.Application/Services/ProjectService.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using TaskManagement.Domain.Dtos.Project;
+using TaskManagement.Domain.Entities;
+using TaskManagement.Domain.Interface.Persistence;
+using TaskManagement.Domain.Interface.Services;
+
+namespace TaskManagement.Application.Services;
+
+public class ProjectService : IProjectService
+{
+    private IMapper Mapper { get; }
+    private IGenericRepository<Project> ProjectRepository { get; }
+
+    public ProjectService(IMapper mapper, IGenericRepository<Project> projectRepository)
+    {
+        Mapper = mapper;
+        ProjectRepository = projectRepository;
+    }
+
+
+
+
+    public async Task<Guid> CreateProjectAsync(ProjectCreate projectCreate)
+    {
+        var entity = Mapper.Map<Project>(projectCreate);
+        await ProjectRepository.InsertAsync(entity);
+        await ProjectRepository.SaveChangesAsync();
+        return entity.Id;
+    }
+}
