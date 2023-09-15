cefccab4c9a71a2ad35bab55cad8cba6594253b4
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2023-09-15 14:50:12 +0100

    add the dto to delete users

---
+++ TaskManagement.Domain/Dtos/User/UserDelete.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagement.Domain.Dtos.User
+{
+    public record UserDelete(Guid Id);
+}
