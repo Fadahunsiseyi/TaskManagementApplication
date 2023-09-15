b4c5d3ff47ee633956b7a01e589ad10db7912edb
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2023-09-15 13:19:49 +0100

    add the update method in the generic interface

--- TaskManagement.Domain/Interface/Persistence/IGenericRepository.cs
+++ TaskManagement.Domain/Interface/Persistence/IGenericRepository.cs
@@ -8,5 +8,6 @@ public interface IGenericRepository<T> where T : BaseEntity
     Task<Guid> InsertAsync(T entity);
     Task<IEnumerable<T>> GetAllAsync(int? skip, int? take);
     Task<T?> GetByIdAsync(Guid id);
+    void Update(T entity);
     System.Threading.Tasks.Task SaveChangesAsync();
 }
