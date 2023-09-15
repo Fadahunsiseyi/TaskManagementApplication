d62443351f5b18eb2535cd636bf01ef563d0ad77
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2023-09-15 01:58:38 +0100

    add the contract to get all users

--- TaskManagement.Domain/Interface/Services/IUserService.cs
+++ TaskManagement.Domain/Interface/Services/IUserService.cs
@@ -5,4 +5,5 @@ namespace TaskManagement.Domain.Interface.Services;
 public interface IUserService
 {
     Task<Guid> CreateUserAsync(UserCreate addressCreate);
+    Task<IEnumerable<UserGet>> GetUsersAsync();
 }
