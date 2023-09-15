29d83030ef6995f4890bec0e4eb6e5287f5c69c6
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2023-09-15 14:55:31 +0100

    implement the method to delete user in the user service

--- TaskManagement.Application/Services/UserService.cs
+++ TaskManagement.Application/Services/UserService.cs
@@ -4,45 +4,51 @@ using TaskManagement.Domain.Entities;
 using TaskManagement.Domain.Interface.Persistence;
 using TaskManagement.Domain.Interface.Services;
 
-namespace TaskManagement.Application.Services
+namespace TaskManagement.Application.Services;
+
+public class UserService : IUserService
 {
-    public class UserService : IUserService
+    private IMapper Mapper { get; }
+    private IGenericRepository<User> UserRepository { get; }
+
+    public UserService(IMapper mapper, IGenericRepository<User> userRepository)
     {
-        private IMapper Mapper { get; }
-        private IGenericRepository<User> UserRepository { get; }
-
-        public UserService(IMapper mapper, IGenericRepository<User> userRepository)
-        {
-            Mapper = mapper;
-            UserRepository = userRepository;
-        }
+        Mapper = mapper;
+        UserRepository = userRepository;
+    }
 
 
 
-        public async Task<Guid> CreateUserAsync(UserCreate addressCreate)
-        {
-            var entity = Mapper.Map<User>(addressCreate);
-            await UserRepository.InsertAsync(entity);
-            await UserRepository.SaveChangesAsync();
-            return entity.Id;
-        }
-        public async Task<IEnumerable<UserGet>> GetUsersAsync()
-        {
-            var entity = await UserRepository.GetAllAsync(null,null);
-            return Mapper.Map<IEnumerable<UserGet>>(entity);
-        }
-        public async Task<UserGet> GetUserAsync(Guid id)
-        {
-            var entity = await UserRepository.GetByIdAsync(id);
-            return Mapper.Map<UserGet>(entity);
-        }
-        public async System.Threading.Tasks.Task UpdateUserAsync(Guid id, UserUpdate userUpdate)
-        {
-            var existingEntity = await UserRepository.GetByIdAsync(id);
-            if (existingEntity is null) throw new Exception("User not found");
-            var entity = Mapper.Map(userUpdate, existingEntity);
-            UserRepository.Update(entity);
-            await UserRepository.SaveChangesAsync();
-        }
+    public async Task<Guid> CreateUserAsync(UserCreate addressCreate)
+    {
+        var entity = Mapper.Map<User>(addressCreate);
+        await UserRepository.InsertAsync(entity);
+        await UserRepository.SaveChangesAsync();
+        return entity.Id;
+    }
+    public async Task<IEnumerable<UserGet>> GetUsersAsync()
+    {
+        var entity = await UserRepository.GetAllAsync(null,null);
+        return Mapper.Map<IEnumerable<UserGet>>(entity);
+    }
+    public async Task<UserGet> GetUserAsync(Guid id)
+    {
+        var entity = await UserRepository.GetByIdAsync(id);
+        return Mapper.Map<UserGet>(entity);
+    }
+    public async System.Threading.Tasks.Task UpdateUserAsync(Guid id, UserUpdate userUpdate)
+    {
+        var existingEntity = await UserRepository.GetByIdAsync(id);
+        if (existingEntity is null) throw new Exception("User not found");
+        var entity = Mapper.Map(userUpdate, existingEntity);
+        UserRepository.Update(entity);
+        await UserRepository.SaveChangesAsync();
+    }
+    public async System.Threading.Tasks.Task DeleteUserAsync(Guid id)
+    {
+        var entity = await UserRepository.GetByIdAsync(id);
+        if (entity is null) throw new Exception("User not found");
+        UserRepository.Delete(entity);
+        await UserRepository.SaveChangesAsync();
     }
 }
