688223e9a96d8d92a930c279594cd19726368ac6
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2023-09-15 08:24:58 +0100

    add contract to get a specific user in the generic repository

--- TaskManagement.Domain/Interface/Persistence/IGenericRepository.cs
+++ TaskManagement.Domain/Interface/Persistence/IGenericRepository.cs
@@ -7,5 +7,6 @@ public interface IGenericRepository<T> where T : BaseEntity
 {
     Task<Guid> InsertAsync(T entity);
     Task<IEnumerable<T>> GetAllAsync(int? skip, int? take);
+    Task<T?> GetByIdAsync(Guid id);
     System.Threading.Tasks.Task SaveChangesAsync();
 }
