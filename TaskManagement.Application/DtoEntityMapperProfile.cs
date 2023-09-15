db4d08497f0d4563404adb733c11bce44e91739c
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2023-09-15 02:02:49 +0100

    add entity user to the dto userget

--- TaskManagement.Application/DtoEntityMapperProfile.cs
+++ TaskManagement.Application/DtoEntityMapperProfile.cs
@@ -1,21 +1,15 @@
 ﻿using AutoMapper;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 using TaskManagement.Domain.Dtos.User;
 using TaskManagement.Domain.Entities;
 
-namespace TaskManagement.Application
+namespace TaskManagement.Application;
+
+public class DtoEntityMapperProfile : Profile
 {
-    public class DtoEntityMapperProfile : Profile
+    public DtoEntityMapperProfile()
     {
-        public DtoEntityMapperProfile()
-        {
-            CreateMap<UserCreate, User>()
-    .ForMember(dest => dest.Id, opt => opt.Ignore());
-        }
+        CreateMap<UserCreate, User>()
+       .ForMember(dest => dest.Id, opt => opt.Ignore());
+        CreateMap<User, UserGet>();
     }
 }
