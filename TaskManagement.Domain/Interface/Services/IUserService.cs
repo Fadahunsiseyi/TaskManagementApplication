ae203870aabe2ce4fb544a5fa39b43fe61a8cce9
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2023-09-15 13:18:59 +0100

    add interface method to update a user

--- TaskManagement.Domain/Interface/Services/IUserService.cs
+++ TaskManagement.Domain/Interface/Services/IUserService.cs
@@ -7,4 +7,5 @@ public interface IUserService
     Task<Guid> CreateUserAsync(UserCreate addressCreate);
     Task<IEnumerable<UserGet>> GetUsersAsync();
     Task<UserGet> GetUserAsync(Guid id);
+    Task UpdateUserAsync(Guid id, UserUpdate userUpdate);
 }
