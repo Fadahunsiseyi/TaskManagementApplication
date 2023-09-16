d3adfed3a2c32d47baaf9889d58d8fbfc40c3efe
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2023-09-16 04:59:00 +0100

    implement the endpoint to create a task in the controller

---
+++ TaskManagement.API/Controllers/TaskController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskManagement.Domain.Dtos.Task;
+using TaskManagement.Domain.Interface.Services;
+
+namespace TaskManagement.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TaskController : ControllerBase
+{
+    public ITaskService TaskService { get; }
+
+    public TaskController(ITaskService taskService)
+    {
+        TaskService = taskService;
+    }
+
+    [HttpPost]
+    [Route("Create")]
+    public async Task<IActionResult> CreateTask(TaskCreate taskCreate)
+    {
+        var id = await TaskService.CreateTaskAsync(taskCreate);
+        return Ok(id);
+    }
+}
