e1e9ea9cda7b70b76e5a27fc8a85c18d5f620398
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2023-09-16 02:08:25 +0100

    implement the methods in the contract for the project service

--- TaskManagement.Application/Services/ProjectService.cs
+++ TaskManagement.Application/Services/ProjectService.cs
@@ -17,14 +17,36 @@ public class ProjectService : IProjectService
         ProjectRepository = projectRepository;
     }
 
-
-
-
     public async Task<Guid> CreateProjectAsync(ProjectCreate projectCreate)
     {
         var entity = Mapper.Map<Project>(projectCreate);
         await ProjectRepository.InsertAsync(entity);
         await ProjectRepository.SaveChangesAsync();
         return entity.Id;
+    }
+    public async Task<IEnumerable<ProjectGet>> GetProjectsAsync()
+    {
+        var entity = await ProjectRepository.GetAllAsync(null,null);
+        return Mapper.Map<IEnumerable<ProjectGet>>(entity);
+    }
+    public async Task<ProjectGet> GetProjectAsync(Guid id)
+    {
+        var entity = await ProjectRepository.GetByIdAsync(id);
+        return Mapper.Map<ProjectGet>(entity);
+    }
+    public async System.Threading.Tasks.Task UpdateProjectAsync(Guid id, ProjectUpdate projectUpdate)
+    {
+        var existingEntity = await ProjectRepository.GetByIdAsync(id);
+        if (existingEntity is null) throw new Exception("Project not found");
+        var entity = Mapper.Map(projectUpdate, existingEntity);
+        ProjectRepository.Update(entity);
+        await ProjectRepository.SaveChangesAsync();
+    }
+    public async System.Threading.Tasks.Task DeleteProjectAsync(Guid id)
+    {
+        var entity = await ProjectRepository.GetByIdAsync(id);
+        if (entity is null) throw new Exception("Project not found");
+        ProjectRepository.Delete(entity);
+        await ProjectRepository.SaveChangesAsync();
     }
 }
