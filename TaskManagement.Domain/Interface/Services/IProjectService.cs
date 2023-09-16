fb8020c84bd716ce93e81492e7666fe44f4ff20f
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2023-09-16 02:07:00 +0100

    implement contract to get,update and delete a project

--- TaskManagement.Domain/Interface/Services/IProjectService.cs
+++ TaskManagement.Domain/Interface/Services/IProjectService.cs
@@ -5,4 +5,8 @@ namespace TaskManagement.Domain.Interface.Services;
 public interface IProjectService
 {
     Task<Guid> CreateProjectAsync(ProjectCreate projectCreate);
+    Task<IEnumerable<ProjectGet>> GetProjectsAsync();
+    Task<ProjectGet> GetProjectAsync(Guid id);
+    Task UpdateProjectAsync(Guid id, ProjectUpdate projectUpdate);
+    Task DeleteProjectAsync(Guid id);
 }
