514849ef8cc69183e19155f208819704913c5c26
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2023-09-16 16:10:03 +0100

    add the dto to get details of each tasks

---
+++ TaskManagement.Domain/Dtos/Task/TaskDetails.cs
@@ -0,0 +1,6 @@
+﻿using TaskManagement.Domain.Dtos.Project;
+using TaskManagement.Domain.Dtos.User;
+
+namespace TaskManagement.Domain.Dtos.Task;
+
+public record TaskDetails(Guid Id, string Title, string Description, UserGet User, ProjectGet Project);
