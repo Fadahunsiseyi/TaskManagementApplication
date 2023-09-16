0d50d9caef1135a16a26fe5413bdea367955014e
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2023-09-16 02:11:24 +0100

    add the endpoints for the project entity crud

--- TaskManagement.API/Controllers/ProjectController.cs
+++ TaskManagement.API/Controllers/ProjectController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TaskManagement.Application.Services;
 using TaskManagement.Domain.Dtos.Project;
 using TaskManagement.Domain.Interface.Services;
 
@@ -21,5 +22,34 @@ public class ProjectController : ControllerBase
     {
         var id = await ProjectService.CreateProjectAsync(projectCreate);
         return Ok(id);
+    }
+    [HttpGet]
+    [Route("Get")]
+    public async Task<IActionResult> GetUsers()
+    {
+        var users = await ProjectService.GetProjectsAsync();
+        return Ok(users);
+    }
+
+    [HttpGet]
+    [Route("Get/{id}")]
+    public async Task<IActionResult> GetUser(Guid id)
+    {
+        var user = await ProjectService.GetProjectAsync(id);
+        return Ok(user);
+    }
+    [HttpPut]
+    [Route("Update/{id}")]
+    public async Task<IActionResult> UpdateUser([FromRoute]Guid id, ProjectUpdate projectUpdate)
+    {
+        await ProjectService.UpdateProjectAsync(id, projectUpdate);
+        return Ok();
+    }
+    [HttpDelete]
+    [Route("Delete/{id}")]
+    public async Task<IActionResult> DeleteUser([FromRoute]Guid id)
+    {
+        await ProjectService.DeleteProjectAsync(id);
+        return Ok();
     }
 }
