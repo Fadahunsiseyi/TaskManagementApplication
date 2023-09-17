0ac254c9ab5b6d522f67c0579bb9e23a90793fa4
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2023-09-17 21:22:07 +0100

    add notification service method contracts

---
+++ TaskManagement.Domain/Interface/Services/INotificationService.cs
@@ -0,0 +1,10 @@
+﻿using TaskManagement.Domain.Dtos.Notification;
+
+namespace TaskManagement.Domain.Interface.Services;
+
+public interface INotificationService
+{
+    public Task<Guid> CreateNotificationAsync(NotificationCreate notificationCreate);
+    public Task<IEnumerable<NotificationGet>> GetNotificationsAsync();
+    public Task<NotificationGet> GetNotificationAsync(Guid id);
+}
