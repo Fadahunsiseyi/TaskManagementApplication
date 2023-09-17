855ff89dbfabc85a2260df8db01ade9cd234a33f
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2023-09-17 07:25:41 +0100

    implement contract to get filtered entities

--- TaskManagement.Domain/Interface/Persistence/IGenericRepository.cs
+++ TaskManagement.Domain/Interface/Persistence/IGenericRepository.cs
@@ -8,6 +8,7 @@ public interface IGenericRepository<T> where T : BaseEntity
     Task<Guid> InsertAsync(T entity);
     Task<IEnumerable<T>> GetAllAsync(int? skip, int? take);
     Task<T?> GetByIdAsync(Guid id, params Expression<Func<T, object>>[] includes);
+    Task<IEnumerable<T>> GetFilteredAsync(Expression<Func<T, bool>>[] filters, params Expression<Func<T, object>>[] includes);
     void Update(T entity);
     void Delete(T entity);
     System.Threading.Tasks.Task SaveChangesAsync();
