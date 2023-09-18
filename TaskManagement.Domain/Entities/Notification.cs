ddba740c3feeca30409a42ad381f25252fe58eb7
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2023-09-18 07:04:10 +0100

    add is read property

--- TaskManagement.Domain/Entities/Notification.cs
+++ TaskManagement.Domain/Entities/Notification.cs
@@ -7,6 +7,7 @@ public class Notification : BaseEntity
     public string Message { get; set; }
     public NotificationsType Type { get; set; }
     public Guid UserId { get; set; }
+    public bool IsRead { get; set; }
 
     public virtual User User { get; set; }
 }
