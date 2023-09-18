6717e452a0fba4e59fb3743ff2658fbc34041a08
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2023-09-18 04:45:53 +0100

    add the method in the notification contract to update and delete notification

--- TaskManagement.Domain/Interface/Services/INotificationService.cs
+++ TaskManagement.Domain/Interface/Services/INotificationService.cs
@@ -7,4 +7,6 @@ public interface INotificationService
     public Task<Guid> CreateNotificationAsync(NotificationCreate notificationCreate);
     public Task<IEnumerable<NotificationGet>> GetNotificationsAsync();
     public Task<NotificationGet> GetNotificationAsync(Guid id);
+    public Task UpdateNotificationAsync(Guid id, NotificationUpdate notificationUpdate);
+    public Task DeleteNotificationAsync(Guid id);
 }
